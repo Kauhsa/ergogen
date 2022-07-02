340f95e6c28d7b6dca0f5de83241e68a582eaf34
Mika Viinamäki <mika@skipperi.fi>
2022-07-02 18:51:58 +0300

    Raspberry Pico support

--- src/footprints/index.js
+++ src/footprints/index.js
@@ -11,6 +11,7 @@ module.exports = {
     omron: require('./omron'),
     pad: require('./pad'),
     promicro: require('./promicro'),
+    raspberrypico: require('./raspberrypico'),
     rgb: require('./rgb'),
     rotary: require('./rotary'),
     scrollwheel: require('./scrollwheel'),
